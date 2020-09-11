6c1a9f4dd6495d3affd41e7faadd21eff415f2ac
Eric Taylor <eric@etaylor.io>
2020-09-11 13:43:02 -0400

    Added some wow assets to test with

--- components/CharacterPane.vue
+++ components/CharacterPane.vue
@@ -15,20 +15,27 @@
         <div class="character-preview">
           <vs-row>
             <vs-col w="12" class="character-pane_title">
-              <div>header</div>
+              <div class="title-slot">
+                <div class="title-name">Accomp</div>
+                <div class="title-class">Assassination Rogue</div>
+                <div class="title-level">Level 120</div>
+              </div>
             </vs-col>
           </vs-row>
           <vs-row>
-            <vs-col w="12" class="character-pane_title">
-              <div>image</div>
+            <vs-col w="12" class="character-pane_preview">
+              <div class="preview-slot"></div>
             </vs-col>
           </vs-row>
           <vs-row>
-            <vs-col w="6" class="character-pane_preview">
-              <div>weap 1</div>
+            <vs-col w="6" class="character-pane_weapon">
+              <div class="weapon-slot"></div>
             </vs-col>
-            <vs-col w="6" class="character-pane_weapons">
-              <div>weap 2</div>
+            <vs-col w="6" class="character-pane_weapon">
+              <vs-tooltip bottom>
+                <div class="weapon-slot"></div>
+                <template #tooltip> This is a WIP. </template>
+              </vs-tooltip>
             </vs-col>
           </vs-row>
         </div>
@@ -55,7 +62,8 @@
   width: calc(100% - 20px);
 }
 .gear-slot {
-  background: rgb(223, 11, 212);
+  background: url('https://i.imgur.com/BxAxImN.png');
+  background-size: cover;
   border: 2px solid #000;
   margin-bottom: 10px;
   padding-top: 100%;
@@ -65,15 +73,33 @@
   }
 }
 .character-pane_title {
-  background: rgb(223, 11, 212);
+  background: rgba(0, 0, 0, 0.8);
   border: 2px solid #000;
+  color: #fff;
+  margin-bottom: 10px;
+}
+.title-slot {
+  text-align: center;
 }
 .character-pane_preview {
-  background: rgb(223, 11, 212);
+  background: url('https://i.imgur.com/qU1w585.jpg') center -550px;
   border: 2px solid #000;
+  margin-bottom: 10px;
 }
-.character-pane_weapons {
+.preview-slot {
+  padding-top: 150%;
+}
+.character-pane_weapon {
+  padding-right: 5px;
+
+  &:last-child {
+    padding-left: 5px;
+    padding-right: 0;
+  }
+}
+.weapon-slot {
   background: rgb(223, 11, 212);
   border: 2px solid #000;
+  padding-top: 50%;
 }
 </style>
